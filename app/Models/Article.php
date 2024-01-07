11e522f03582b8d3f4a829f43e5d631c597be3aa
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-07 07:03:38 +0000

    adds model for article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $table = 'articles';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['title', 'excerpt', 'body'];
 }

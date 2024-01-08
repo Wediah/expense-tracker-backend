08a221489d60355ab3149c4c3ac93137c85764d2
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-08 15:37:29 +0000

    adds browser validation to forms

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -12,11 +12,11 @@
                     <label class="label" for="title">Title</label>
 
                     <div class="control">
-                        <input class="input" type="text" name="title" id="title">
+                        <input class="input @error('title') is-danger @enderror" type="text" name="title" id="title" value="{{ old('title') }}">
 
-                        @if ($errors->has('title'))
+                        @error('title')
                             <p class="help is-danger">{{ $errors->first('title') }}</p>
-                        @endif
+                        @enderror
                     </div>
                 </div>
 
@@ -24,7 +24,11 @@
                     <label class="label" for="excerpt">Excerpt</label>
 
                     <div class="control">
-                        <textarea class="textarea"  name="excerpt" id="excerpt"></textarea>
+                        <textarea class="textarea @error('excerpt') is-danger @enderror"  name="excerpt" id="excerpt" value="{{ old('excerpt') }}"></textarea>
+
+                        @error('excerpt')
+                            <p class="help is-danger">{{ $errors->first('excerpt') }}</p>
+                        @enderror
                     </div>
                 </div>
 
@@ -32,7 +36,11 @@
                     <label class="label" for="body">Body</label>
 
                     <div class="control">
-                        <textarea class="textarea" name="body" id="body"></textarea>
+                        <textarea class="textarea @error('body') is-danger @enderror" name="body" id="body" value="{{ old('body') }}"></textarea>
+
+                        @error('body')
+                            <p class="help is-danger">{{ $errors->first('body') }}</p>
+                        @enderror
                     </div>
                 </div>
 

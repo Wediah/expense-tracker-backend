62b69243cdd17f9750cc8dbb52b89013019948d9
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-08 19:38:37 +0000

    adds route model binding to store method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -26,20 +26,11 @@ class ArticlesController extends Controller
 
     public function store()
     {
-
-        request()->validate([
+        Article::create(request()->validate([
             'title' => 'required',
             'excerpt' => 'required',
             'body' => 'required',
-        ]);
-
-        $article = new Article();
-
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
-
-        $article->save();
+        ]));
 
         return redirect('/articles');
     }
@@ -51,17 +42,12 @@ class ArticlesController extends Controller
 
     public function update(Article $article)
     {
-        request()->validate([
+        $article->update(request()->validate([
             'title' => 'required',
             'excerpt' => 'required',
             'body' => 'required',
-        ]);
+        ]));
 
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
-
-        $article->save();
 
         return redirect('/articles/' . $article->id);
     }
